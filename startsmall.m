95263495b08f0203093c56ee4fa30f39455df8b8
BerfinBastug <berfin.bastug@gmail.com>
2022-02-07 09:19:57 +0100

    DATA COLLECTION MODE

--- startsmall.m
+++ startsmall.m
@@ -1,6 +1,6 @@
-try
+function startsmall(subID, blockID)
     input ('start>>>','s'); % prints to command window
-    
+try
     PsychDefaultSetup(1); % executes the AssertOpenGL & KbName('UnifyKeyNames')
     
     rand('seed', sum(100 * clock));
@@ -42,187 +42,161 @@ try
     yCoords = [0 0 -fixCrossDimPix fixCrossDimPix];
     allCoords = [xCoords; yCoords];
     
-    lineWidthPix = 4; % Set the line width for our fixation cross
-    
-    %STEP8: TRIAL NAMES
-    FRtrialinfo = ['FREE RESPONSE TRIAL\n' ...
-               'Respond as soon as possible \n'];
-    VSDtrialinfo =['VARIED STIMULUS DURATION TRIAL\n' ...
-               'Respond when the sound finishes \n']; 
-           
+    lineWidthPix = 4; % Set the line width for our fixation cross     
     %% RANDOMIZE FRstim
-    % STEP1: LOCATE WHERE THE SOUNDS ARE
-    FRstimDir = '/Users/admin/Desktop/m2project/FRstim';
-    FRstims=dir(FRstimDir);
-    FRstim_list = {FRstims.name};
-
-    % STEP2: DROP THE EMPTY ITEMS IN THE LIST
-    FRstim_list = FRstim_list(~ismember(FRstim_list,{'.','..'}));
-    FRstim_list = convertCharsToStrings(FRstim_list);
-
-    % STEP3: RANDOMIZE THE SOUNDS
-    FRnum_stims = length(FRstim_list);
-    FRstim_indices = 1:FRnum_stims;
-    FRnum_repeats = 1;
-    FRnum_trials = FRnum_stims * FRnum_repeats;
-    FRrand_stim_order = nan(1, FRnum_trials);
-
-    for k = 1:FRnum_stims:FRnum_trials
-        subsequence = Shuffle(FRstim_indices);
-        FRrand_stim_order(k:k+(FRnum_stims-1)) = subsequence;
-        % swap the first value of this subsequence if it repeats the previous
-        if k > 1 && (FRrand_stim_order(k) == FRrand_stim_order(k-1))
-            FRrand_stim_order([k, k+1]) = FRrand_stim_order([k+1, k]);
-        end
-    end
+    [FRBLOCKS] = organizeFRblocks;
     
     %% RANDOMIZE VSDstim
-    % STEP1: LOCATE WHERE THE SOUNDS ARE
-    VSDstimDir = '/Users/admin/Desktop/m2project/VSDstim';
-    VSDstims=dir(VSDstimDir);
-    VSDstim_list = {VSDstims.name};
-
-    % STEP2: DROP THE EMPTY ITEMS IN THE LIST
-    VSDstim_list = VSDstim_list(~ismember(VSDstim_list,{'.','..'}));
-    VSDstim_list = convertCharsToStrings(VSDstim_list);
-
-    % STEP3: RANDOMIZE THE SOUNDS
-    VSDnum_stims = length(VSDstim_list);
-    VSDstim_indices = 1:VSDnum_stims;
-    VSDnum_repeats = 1;
-    VSDnum_trials = VSDnum_stims * VSDnum_repeats;
-    VSDrand_stim_order = nan(1, VSDnum_trials);
-
-    for k = 1:VSDnum_stims:VSDnum_trials
-        subsequence = Shuffle(VSDstim_indices);
-        VSDrand_stim_order(k:k+(VSDnum_stims-1)) = subsequence;
-        % swap the first value of this subsequence if it repeats the previous
-        if k > 1 && (VSDrand_stim_order(k) == VSDrand_stim_order(k-1))
-            VSDrand_stim_order([k, k+1]) = VSDrand_stim_order([k+1, k]);
-        end
-    end
-    %% RANDOMIZE TASKS
+    [VSDBLOCKS] = organizeVSDblocks;
     
+    %% RANDOMIZE TASKS
     taskFR = "FR";
     taskVSD = "VSD";
+    
+    FRtrialnum = 10;
+    VSDtrialnum = 10;
 
-    TASKS = repelem([taskFR, taskVSD], [3 3]);
+    TASKS = repelem([taskFR, taskVSD], [FRtrialnum VSDtrialnum]);
     s_TASKS = Shuffle(TASKS);
     
-    %% NOTE DOWN THE USED STIMS
-    FRusedsounds = FRstim_list(FRrand_stim_order);
-    FRusedsounds= FRusedsounds(1:length(taskFR));
-    
-    VSDusedsounds = VSDstim_list(VSDrand_stim_order);
-    VSDusedsounds= VSDusedsounds(1:length(taskVSD));
+    HideCursor;
     %% PRESENTATION OF TRIALS
     
+    FRconstant = 1;
+    VSDconstant = 1;
+    
     for i=1:length(s_TASKS)
-        WaitSecs(0.3);
         
-        thiscond = s_TASKS(i);
-        if thiscond == "FR"
-            for a=1:length(FRrand_stim_order)
-                
-                % STEP1: CALL THE SOUND
-                DrawFormattedText(windowPtr, FRtrialinfo, 'center', 'center');
-                tFR(a) = Screen('Flip', windowPtr); 
-                
-                FRused(a) = FRstim_list(FRrand_stim_order(a));
-                whichFRstim = FRstim_list(FRrand_stim_order(a));
-                [y,freq] = psychwavread(whichFRstim);
-
-                % STEP5: LEARN ITS DURATION
-                stimDurFR(a) = length(y)./freq;
-
-                % STEP6: GET VALUES NECESSARY TO ENTER INTO THE PSYCHPORT FUNCTIONS
-                wave = y'; %be careful it is transposed
-                nrchannels = size(wave,1);
-
-                % STEP7: MAKE SURE IT HAS 2 CHANNELS
-                if nrchannels <2 
-                    wave = [wave ; wave];
-                    nrchannels = 2;
-                end
-
-                % STEP8: OPEN THE PSYCHPORT AUDIO
-                pahandle = PsychPortAudio('Open', [], [], 0, freq, nrchannels);
-
-                % STEP9: FILL THE BUFFER
-                PsychPortAudio('FillBuffer',pahandle,wave);
-
-                % STEP10: SETTING THE PARAMETERS FOR PSYCHPORT AUDIO START
-                repetitions = 1;
-                when = 0; % Start immediately (0 = immediately)
-                waitForDeviceStart = 1; % wait for start argument
-
-                % STEP11: KEYPRESS WILL STOP THE PLAYBACK
-                t1 = PsychPortAudio('Start', pahandle, repetitions, when, waitForDeviceStart);
-
-                KbReleaseWait; % Wait for release of all keys on keyboard:
-
-                lastSample = 0;
-                lastTime = t1;
-
-                while ~KbCheck % Stay in a little loop until keypress:
-                    s = PsychPortAudio('GetStatus', pahandle);
-                    realSampleRate = (s.ElapsedOutSamples - lastSample) / (s.CurrentStreamTime - lastTime);
-                    tHost = s.CurrentStreamTime;
-                end
-
-                [~,secs,keyCode] = KbCheck;
-                if keyCode(spaceKey)
-                    RESPONSE_FR(a) = 1;
-                    RT_FR(a) = secs-t1;
-                elseif keyCode(nKey)
-                    RESPONSE_FR(a) = 0;
-                    RT_FR(a) = secs-t1;
-                end
-
-                % STEP12: STOP THE AUDIO 
-                [startTime,~,~, stopTime] = PsychPortAudio('Stop', pahandle);
-
-                % STEP13: CLOSE THE AUDIO DEVICE
-                PsychPortAudio('Close', pahandle);
-                FRstim_list(strcmp(FRstim_list, whichFRstim)) = [];
-            end
+        block.subID(i) = subID;
+        block.blockID(i) = blockID;
+        
+        WaitSecs(0.5);
+        
+        block.thiscond(i) = s_TASKS(i);
+        
+        if block.thiscond(i) == "FR"
+        
+            % STEP1: GIVE THE TRIAL INFORMATION
+            FRinfo = ['Trial no: ' num2str(i) '\n' ...
+                  'Free response trial\n' ...
+                  ' \n' ...
+                  ' \n' ...
+                  'Was the same sound played multiple times?\n' ...
+                  ' \n' ...
+                  'Press  [space bar]  if the answer is yes \n' ...
+                  'Press  [n]  if the answer is no\n' ...
+                  'You can respond as soon as you decide\n'];
+            DrawFormattedText(windowPtr, FRinfo, 'center', 'center');
+            tFR(FRconstant) = Screen('Flip', windowPtr); 
             
-        elseif thiscond == "VSD"
-            for b=1:length(VSDrand_stim_order)
-
-            % STEP4: CALL THE SOUND
-            DrawFormattedText(windowPtr, VSDtrialinfo, 'center', 'center');
-            tVSD(b) = Screen('Flip', windowPtr);
+            % STEP2: CALL THE SOUND
+            whichFRstim = FRBLOCKS(blockID).FR(FRconstant);
+            [y,freq] = psychwavread(whichFRstim);
             
-            VSDused = VSDstim_list(VSDrand_stim_order(b));
-            whichVSDstim= VSDstim_list(VSDrand_stim_order(b));
-            [y,freq] = psychwavread(whichVSDstim);
+            % STEP3: LEARN ITS NAME
+            block.Stim(i) = convertCharsToStrings(whichFRstim);
 
-            % STEP5: LEARN ITS DURATION
-            stimDurVSD(b) = length(y)./freq;
+            % STEP4: LEARN ITS DURATION
+            block.stimDur(i) = length(y)./freq;
 
-            % STEP6: GET VALUES NECESSARY TO ENTER INTO THE PSYCHPORT FUNCTIONS
+            % STEP5: GET VALUES NECESSARY TO ENTER INTO THE PSYCHPORT FUNCTIONS
             wave = y'; %be careful it is transposed
             nrchannels = size(wave,1);
 
-            % STEP7: MAKE SURE IT HAS 2 CHANNELS
+            % STEP6: MAKE SURE IT HAS 2 CHANNELS
             if nrchannels <2 
                 wave = [wave ; wave];
                 nrchannels = 2;
             end
 
-            % STEP8: OPEN THE PSYCHPORT AUDIO
+            % STEP7: OPEN THE PSYCHPORT AUDIO
             pahandle = PsychPortAudio('Open', [], [], 0, freq, nrchannels);
 
-            % STEP9: FILL THE BUFFER
+            % STEP8: FILL THE BUFFER
             PsychPortAudio('FillBuffer',pahandle,wave);
 
-            % STEP10: SETTING THE PARAMETERS FOR PSYCHPORT AUDIO START
+            % STEP9: SETTING THE PARAMETERS FOR PSYCHPORT AUDIO START
             repetitions = 1;
             when = 0; % Start immediately (0 = immediately)
             waitForDeviceStart = 1; % wait for start argument
 
-            % STEP11: WAIT TILL SOUND IS PLAYED
+            % STEP10: KEYPRESS WILL STOP THE PLAYBACK
+            t1 = PsychPortAudio('Start', pahandle, repetitions, when, waitForDeviceStart);
+
+            KbReleaseWait; % Wait for release of all keys on keyboard:
+
+            lastSample = 0;
+            lastTime = t1;
+
+            while ~KbCheck % Stay in a little loop until keypress:
+                s = PsychPortAudio('GetStatus', pahandle);
+                realSampleRate = (s.ElapsedOutSamples - lastSample) / (s.CurrentStreamTime - lastTime);
+                tHost = s.CurrentStreamTime;
+            end
+
+            [~,secs,keyCode] = KbCheck;
+            if keyCode(spaceKey)
+                block.RESPONSE(i) = 1;
+                block.RT(i) = secs-t1;
+            elseif keyCode(nKey)
+                block.RESPONSE(i) = 0;
+                block.RT(i) = secs-t1;
+            end
+
+            % STEP11: STOP THE AUDIO 
+            [startTime,~,~, stopTime] = PsychPortAudio('Stop', pahandle);
+
+            % STEP12: CLOSE THE AUDIO DEVICE
+            PsychPortAudio('Close', pahandle);
+            FRconstant = FRconstant + 1;
+        
+        elseif block.thiscond(i) == "VSD"
+
+            % STEP1: GIVE THE TRIAL INFORMATION
+            VSDinfo = ['Trial no: ' num2str(i) '\n' ...
+                  'Varied stimulus duration trial\n' ...
+                  ' \n' ...
+                  ' \n' ...
+                  'Was the same sound played multiple times?\n' ...
+                  ' \n' ...
+                  'Press  [space bar]  if the answer is yes \n' ...
+                  'Press  [n]  if the answer is no\n' ...
+                  'You can respond after the stimulus completely finishes \n'];
+            DrawFormattedText(windowPtr, VSDinfo, 'center', 'center');
+            tVSD(VSDconstant) = Screen('Flip', windowPtr);
+            
+            % STEP2: CALL THE SOUND
+            whichVSDstim= VSDBLOCKS(blockID).VSD(VSDconstant);
+            [y,freq] = psychwavread(whichVSDstim);
+            
+            % STEP3: LEARN ITS NAME
+            block.Stim(i) = convertCharsToStrings(whichVSDstim);
+            
+            % STEP4: LEARN ITS DURATION
+            block.stimDur(i) = length(y)./freq;
+
+            % STEP5: GET VALUES NECESSARY TO ENTER INTO THE PSYCHPORT FUNCTIONS
+            wave = y'; %be careful it is transposed
+            nrchannels = size(wave,1);
+
+            % STEP6: MAKE SURE IT HAS 2 CHANNELS
+            if nrchannels <2 
+                wave = [wave ; wave];
+                nrchannels = 2;
+            end
+
+            % STEP7: OPEN THE PSYCHPORT AUDIO
+            pahandle = PsychPortAudio('Open', [], [], 0, freq, nrchannels);
+
+            % STEP8: FILL THE BUFFER
+            PsychPortAudio('FillBuffer',pahandle,wave);
+
+            % STEP9: SETTING THE PARAMETERS FOR PSYCHPORT AUDIO START
+            repetitions = 1;
+            when = 0; % Start immediately (0 = immediately)
+            waitForDeviceStart = 1; % wait for start argument
+
+            % STEP10: WAIT TILL SOUND IS PLAYED
             t1 = PsychPortAudio('Start', pahandle, repetitions, when, waitForDeviceStart);
             [actualStartTime, ~, ~, ~] = PsychPortAudio('Stop', pahandle, 1, 1);
 
@@ -232,24 +206,31 @@ try
             while resp == 0 %while there is no logged response
                 [keyIsDown,secs,keyCode] = KbCheck; %continuously checks the state of the keyboard
                 if keyCode(spaceKey) % if space key is pressed
-                    RT(b) = secs - t1; %get the current time and subtract offset time to get the RT
-                    response(b) = 1; %record a repeating response
+                    block.RT(i) = secs - t1; %get the current time and subtract offset time to get the RT
+                    block.RESPONSE(i) = 1; %record a repeating response
                     resp = 1; %exit loop
                 elseif keyCode(nKey) %  if right is pressed
-                    RT(b) = secs - t1; %get the current time and subtract offset time to get the RT
-                    response(b) = 0; %record a repeating response
+                    block.RT(i) = secs - t1; %get the current time and subtract offset time to get the RT
+                    block.RESPONSE(i) = 0; %record a repeating response
                     resp=1; %exit loop
                 end
             end
 
-            % STEP12: CLOSE THE AUDIO DEVICE
+            % STEP11: CLOSE THE AUDIO DEVICE
             PsychPortAudio('Close', pahandle);
-            VSDstim_list(strcmp(VSDstim_list, whichVSDstim)) = [];
-            end
-        end
+            VSDconstant = VSDconstant + 1;
+        end   
     end
     Screen('CloseAll')
+    
+    B_cell = struct2cell(block);
+    B_fields = fieldnames(block);
+    B = [B_fields, B_cell];
+    B_table = cell2table(B);
+    data_name = strcat(num2str(subID),'_',num2str(blockID),'_DATA.csv');
+    writetable(B_table, data_name)
 catch
     Screen('CloseAll')
     psychrethrow(psychlasterror);
+end
 end
